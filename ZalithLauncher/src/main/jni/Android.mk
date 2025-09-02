18a9b5e13a7fd5a045864fe133c4b76dde1d1e59
StevenAnoymous <realvisionmc@proton.me>
2025-09-02 17:26:10 +0630

    I forgot one more thing sorry,

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -96,10 +96,11 @@ include $(BUILD_SHARED_LIBRARY)
 
 # EGLBridge library, used for compatibility with pure ES contexts
 include $(CLEAR_VARS)
-LOCAL_MODULE := eglbridge
-LOCAL_LDLIBS := -ldl
+LOCAL_PATH := $(HERE_PATH)/eglwrap
+LOCAL_MODULE := egl_wrapper
+LOCAL_LDLIBS := -ldl -lEGL
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_SRC_FILES := egl_bridge.c
+LOCAL_SRC_FILES := egl_wrapper.c
 include $(BUILD_SHARED_LIBRARY)
 
 
