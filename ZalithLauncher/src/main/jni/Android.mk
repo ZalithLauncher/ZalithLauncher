8c78bf8706ae389d0e1b0c569bc27dd7c11cea6c
StevenAnoymous <realvisionmc@proton.me>
2025-09-02 13:11:54 +0630

    will do it later

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -94,13 +94,7 @@ LOCAL_SHARED_LIBRARIES := awt_headless
 LOCAL_SRC_FILES := xawt_fake.c
 include $(BUILD_SHARED_LIBRARY)
 
-# GLESBridge library, used for compatibility with pure ES contexts
-include $(CLEAR_VARS)
-LOCAL_MODULE := glesbridge
-LOCAL_LDLIBS := -ldl
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_SRC_FILES := egl_bridge.c
-include $(BUILD_SHARED_LIBRARY)
+// waitttt
 
 
 
