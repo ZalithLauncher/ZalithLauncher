a5de6cb38f02a818486ffd10165e88fec4a7ff3d
StevenAnoymous <realvisionmc@proton.me>
2025-09-02 17:57:48 +0630

    another fix typo

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/GLES3Renderer.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/GLES3Renderer.kt
@@ -13,5 +13,5 @@ class GL4ESRenderer : RendererInterface {
 
     override fun getDlopenLibrary(): Lazy<List<String>> = lazy { emptyList() }
 
-    override fun getRendererLibrary(): String = "libEGL.so"
+    override fun getRendererLibrary(): String = "egl_wrapper.so"
 }
