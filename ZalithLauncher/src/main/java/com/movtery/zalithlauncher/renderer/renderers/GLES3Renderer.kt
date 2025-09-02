d13c5e600b7d0c4f38ec86486a24adf419bd76fd
StevenAnoymous <realvisionmc@proton.me>
2025-09-02 12:46:31 +0630

    add EGL/GLES renderer

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/GLES3Renderer.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/renderer/renderers/GLES3Renderer.kt
@@ -1,1 +1,17 @@
+package com.movtery.zalithlauncher.renderer.renderers
 
+import com.movtery.zalithlauncher.renderer.RendererInterface
+
+class GL4ESRenderer : RendererInterface {
+    override fun getRendererId(): String = "opengles3"
+
+    override fun getUniqueIdentifier(): String = "988f31e9-1aa0-4902-a447-b472c1be52ba"
+
+    override fun getRendererName(): String = "GLES3"
+
+    override fun getRendererEnv(): Lazy<Map<String, String>> = lazy { emptyMap() }
+
+    override fun getDlopenLibrary(): Lazy<List<String>> = lazy { emptyList() }
+
+    override fun getRendererLibrary(): String = "libEGL.so"
+}
